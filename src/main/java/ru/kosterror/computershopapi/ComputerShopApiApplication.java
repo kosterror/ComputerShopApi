9beb5067bcbd3138704b7f5a943a0714e671178e
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-15 11:24:18 +0700

    Инициализировал проект

---
+++ src/main/java/ru/kosterror/computershopapi/ComputerShopApiApplication.java
@@ -0,0 +1,13 @@
+package ru.kosterror.computershopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerShopApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kosterror/computershopapi/ComputerShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kosterror.computershopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
