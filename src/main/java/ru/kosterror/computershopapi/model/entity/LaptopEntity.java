93ae3dc59ec991bf8508d5c3d8c232dd5eb90b40
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-16 11:31:03 +0700

    реализация enum LaptopType и реализация LaptopEntity

---
+++ src/main/java/ru/kosterror/computershopapi/model/entity/LaptopEntity.java
@@ -0,0 +1,32 @@
+package ru.kosterror.computershopapi.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.kosterror.computershopapi.model.enumeration.LaptopType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "laptop")
+@Data
+@NoArgsConstructor
+public class LaptopEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "producer", nullable = false)
+    private String producer;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "count_in_stock", nullable = false)
+    private Long countInStock;
+
+    @Column(name = "screen_diagonal", nullable = false)
+    private LaptopType laptopType;
+
+}
