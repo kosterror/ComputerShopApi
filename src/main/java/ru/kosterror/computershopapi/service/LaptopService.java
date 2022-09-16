7dc506d9aed1507bd395c87941ea99eecbce57cb
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-16 12:29:18 +0700

    Изменил mapping у нескольких запросов

--- src/main/java/ru/kosterror/computershopapi/service/LaptopService.java
+++ src/main/java/ru/kosterror/computershopapi/service/LaptopService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import ru.kosterror.computershopapi.model.dto.CreateLaptopDto;
 import ru.kosterror.computershopapi.model.dto.GetUpdateLaptopDto;
 import ru.kosterror.computershopapi.model.entity.LaptopEntity;
-import ru.kosterror.computershopapi.repository.LaptopRepository;
+import ru.kosterror.computershopapi.model.repository.LaptopRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,5 +43,4 @@ public class LaptopService {
 
         return laptopDtoList;
     }
-
 }
