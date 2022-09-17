640ee7a5b4d183d70a5b5172b5ab20c45cededc7
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-17 22:34:15 +0700

    Реализовал методы для MonitorService: getAll, update, deleteById

--- src/main/java/ru/kosterror/computershopapi/service/MonitorService.java
+++ src/main/java/ru/kosterror/computershopapi/service/MonitorService.java
@@ -5,9 +5,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.kosterror.computershopapi.exceptions.ProductNotFoundException;
 import ru.kosterror.computershopapi.model.dto.CreateLaptopDto;
+import ru.kosterror.computershopapi.model.dto.GetUpdateLaptopDto;
 import ru.kosterror.computershopapi.model.dto.GetUpdateMonitorDto;
 import ru.kosterror.computershopapi.model.entity.MonitorEntity;
 import ru.kosterror.computershopapi.model.repository.MonitorRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +34,39 @@ public class MonitorService {
 
             return modelMapper.map(monitorEntity, GetUpdateMonitorDto.class);
         } catch (Exception e) {
+            throw new ProductNotFoundException("The product with this ID was not found");
+        }
+    }
+
+    public List<GetUpdateMonitorDto> getAll(){
+        List<MonitorEntity> monitorEntities = monitorRepository.findAll();
+
+        List<GetUpdateMonitorDto> monitorDtoList = new ArrayList<>();
+
+        for (MonitorEntity monitorEntity : monitorEntities){
+            monitorDtoList.add(modelMapper.map(monitorEntity, GetUpdateMonitorDto.class));
+        }
+
+        return monitorDtoList;
+    }
+
+    public GetUpdateMonitorDto update(GetUpdateLaptopDto updateLaptopDto){
+        if (monitorRepository.existsById(updateLaptopDto.getId())){
+            MonitorEntity entity = modelMapper.map(updateLaptopDto, MonitorEntity.class);
+            entity = monitorRepository.save(entity);
+
+            return modelMapper.map(entity, GetUpdateMonitorDto.class);
+
+
+        } else{
+            throw new ProductNotFoundException("The product with this ID was not found");
+        }
+    }
+
+    public void deleteById(Long id){
+        try{
+            monitorRepository.deleteById(id);
+        } catch (Exception e){
             throw new ProductNotFoundException("The product with this ID was not found");
         }
     }
