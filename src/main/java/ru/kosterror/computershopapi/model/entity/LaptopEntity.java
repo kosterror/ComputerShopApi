55771e094888d9bc89ad5015661f2b1bf1c76809
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-17 07:19:43 +0700

    Изменил стратегию генерации ID для сущностей в БД

--- src/main/java/ru/kosterror/computershopapi/model/entity/LaptopEntity.java
+++ src/main/java/ru/kosterror/computershopapi/model/entity/LaptopEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class LaptopEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
