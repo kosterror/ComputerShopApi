3b7d21e4538c20b139a167c0e37da3d02b074cf2
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-20 00:31:51 +0700

    Удалил все лишнее

--- src/main/java/ru/kosterror/computershopapi/model/enumeration/ComputerType.java
+++
@@ -1,7 +0,0 @@
-package ru.kosterror.computershopapi.model.enumeration;
-
-public enum ComputerType {
-    DESKTOP,
-    NETTOP,
-    MONOBLOCK
-}
--- src/main/java/ru/kosterror/computershopapi/model/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.kosterror.computershopapi.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.kosterror.computershopapi.model.entity.LaptopEntity;
-
-public interface LaptopRepository extends CrudRepository<LaptopEntity, Long> {
-
-    LaptopEntity getLaptopEntityById(Long id);
-}
