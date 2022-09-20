5d97b2f10343d197cee5a30cba7f943d7a42da8d
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-09-20 22:46:38 +0700

    Описание к MonitorController

--- src/main/java/ru/kosterror/computershopapi/controller/MonitorController.java
+++ src/main/java/ru/kosterror/computershopapi/controller/MonitorController.java
@@ -1,5 +1,6 @@
 package ru.kosterror.computershopapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.kosterror.computershopapi.model.dto.CreateMonitorDto;
@@ -16,26 +17,31 @@ public class MonitorController {
     private final MonitorService monitorService;
 
     @PostMapping
+    @Operation(summary = "Добавление товара")
     public MonitorDto create(@RequestBody CreateMonitorDto monitorDto){
         return monitorService.create(monitorDto);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получение товара")
     public MonitorDto getById(@PathVariable Long id){
         return monitorService.getById(id);
     }
 
     @GetMapping
+    @Operation(summary = "Получить все товары")
     public List<MonitorDto> getAll(){
         return monitorService.getAll();
     }
 
     @PutMapping
+    @Operation(summary = "Изменить данные товара")
     public MonitorDto update(@RequestBody MonitorDto dto){
         return monitorService.update(dto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удалить товар")
     public void delete(@PathVariable Long id){
         monitorService.deleteById(id);
     }
